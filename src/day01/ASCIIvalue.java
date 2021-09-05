0d48bc672f7facbabbe99f413ee6dbb1ecee9f71
Prathmesh Sanjay Shevale <shevaleprathmesh8@gmail.com>
2021-09-05 11:38:39 +0530

    Day01(Yesterday's Update)

---
+++ src/day01/ASCIIvalue.java
@@ -0,0 +1,11 @@
+package day01;
+import java.util.Scanner;
+
+public class ASCIIvalue {
+
+	public static void main(String[] args) {
+		int ch1 = 'a';
+		System.out.println("The ASCII value of a is: "+ch1);
+	}
+
+}
--- src/day01/DecimalMultiplication.java
+++ src/day01/DecimalMultiplication.java
@@ -1,10 +1,17 @@
 package day01;
 
+import java.util.Scanner;
+
 public class DecimalMultiplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter First number");
+		float a=scn.nextFloat();
+		System.out.println("Enter Second Number");
+		float b=scn.nextFloat();
+		System.out.println("Answer-"+a*b);
+		scn.close();
 	}
 
 }
